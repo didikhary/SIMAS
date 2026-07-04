9b4986d0075e4e186d6fcf56bb70b068da266443
Hary Didik Kurniawan <didikhary824@gmail.com>
2026-07-04 17:29:07 +0700

    Modul 2.1 - Penyempurnaan Dashboard Premium

--- js/dashboard.js
+++ js/dashboard.js
@@ -10,3 +10,76 @@ now.toLocaleTimeString("id-ID");
 setInterval(updateClock,1000);
 
 updateClock();
+
+// Simulasi data statistik
+const statistik = {
+
+    siswa:356,
+
+    guru:24,
+
+    kelas:12,
+
+    hadir:325
+
+};
+
+document.getElementById("totalSiswa").innerHTML = statistik.siswa;
+
+document.getElementById("totalGuru").innerHTML = statistik.guru;
+
+document.getElementById("totalKelas").innerHTML = statistik.kelas;
+
+document.getElementById("hadirHariIni").innerHTML = statistik.hadir;
+
+const ctx = document.getElementById('attendanceChart');
+
+new Chart(ctx, {
+
+    type: 'bar',
+
+    data: {
+
+        labels: [
+
+            'Sen',
+
+            'Sel',
+
+            'Rab',
+
+            'Kam',
+
+            'Jum',
+
+            'Sab'
+
+        ],
+
+        datasets: [{
+
+            label: 'Jumlah Kehadiran',
+
+            data: [320,335,340,330,325,310]
+
+        }]
+
+    },
+
+    options: {
+
+        responsive:true,
+
+        plugins:{
+
+            legend:{
+
+                display:false
+
+            }
+
+        }
+
+    }
+
+});
